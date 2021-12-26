b5821f4ee74dd7ee5493efceb80032afd763a603
Abdelrahman Qasim <abdelrahman.qasim@orange.com>
2021-12-26 19:45:05 +0200

    Initial Commit

---
+++ AirtelApp/AirtelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirtelAppApp.swift
+//  AirtelApp
+//
+//  Created by AbdelrahmanQasim on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct AirtelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
